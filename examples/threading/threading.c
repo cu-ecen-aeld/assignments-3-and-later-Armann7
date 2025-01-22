be0b31318d30f22049c4b573798d15e19f24feff
Slava <vyacheslav.v.kuzmin@gmail.com>
2025-01-22 22:34:32 +0100

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -8,12 +8,26 @@
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
+
 void* threadfunc(void* thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    printf("%d\n", thread_func_args->wait_to_obtain_ms);
+    printf("%d\n", thread_func_args->wait_to_release_ms);
+    struct timespec ts;
+    ts.tv_sec = thread_func_args->wait_to_obtain_ms / 1000;
+    ts.tv_nsec = (thread_func_args->wait_to_obtain_ms % 1000) * 1000000;
+    nanosleep(&ts, &ts);
+    pthread_mutex_lock(thread_func_args->mutex);
+    ts.tv_sec = thread_func_args->wait_to_release_ms / 1000;
+    ts.tv_nsec = (thread_func_args->wait_to_release_ms % 1000) * 1000000;
+    nanosleep(&ts, &ts);
+    pthread_mutex_unlock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +42,16 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
 
+    struct thread_data* thread_param = malloc(sizeof(struct thread_data));
+    if (thread_param == NULL)
+        return false;
+    thread_param->thread_complete_success = false;
+    thread_param->mutex = mutex;
+    thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_param->wait_to_release_ms = wait_to_release_ms;
+    int rc = pthread_create(thread, NULL, threadfunc, thread_param);
+    if (rc != 0)
+        return false;
+    return true;
+}
